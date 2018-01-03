1599100b56be46d55d238fc7613a4c904ac87c43
Jens Oberender <burberius@gmail.com>
2018-01-03 22:08:47 +0100

    Initial commit of spring generated code.

---
+++ src/main/java/net/troja/picoro/PiCOffeeROasterApplication.java
@@ -0,0 +1,12 @@
+package net.troja.picoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiCOffeeROasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiCOffeeROasterApplication.class, args);
+	}
+}
